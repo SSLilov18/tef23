73ca8ef80d8528a441d475a772a5e3b7be46edfb
Стивън Лилов <SSLilov18@codingburgas.bg>
2022-11-30 14:40:19 +0200

    any

---
+++ src/main/java/com/example/tef23/Tef23Application.java
@@ -0,0 +1,13 @@
+package com.example.tef23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tef23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tef23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tef23/Tef23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tef23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tef23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
